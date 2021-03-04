f1f4ae26f6840735e1d659c79348de9c89a67dc7
Борис Городецкий <boria20001@gmail.com>
2021-03-04 21:17:15 +0500

    Задача на закон движения

---
+++ Traectory/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Traectory
+{
+    class Program
+    {
+		
+static void Main(string[] args)
+        {
+			double x, y, xmax; //координаты
+			double t, v, a; //время, скорость, угол
+			Console.WriteLine("введите время");
+			t = Convert.ToDouble( Console.ReadLine());
+			Console.WriteLine("введите скорость");
+			v = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("введите угол");
+			a = Convert.ToDouble(Console.ReadLine());
+			x = v * Math.Cos(a * (Math.PI / 180)) * t;
+			y = v * Math.Sin(a * (Math.PI / 180)) * t - 9.8 * t*t / 2;
+			xmax = v*v * Math.Sin(2 * a * (Math.PI / 180)) / 9.8;
+			if (x > xmax)
+			{
+				Console.WriteLine( "X равен: " + xmax );
+				Console.WriteLine("Y равен:0");
+			}
+			else
+			{
+				Console.WriteLine("X равен: " + x );
+				Console.WriteLine("Y равен: " + y);
+			}
+        }
+    }
+}
