3575c75d84a421d40d3fdab8e979d4ac6ddc5585
Boris2001 <72463401+Boris2001@users.noreply.github.com>
2021-04-16 21:53:37 +0500

    добавление в  файл

--- Traectory/Program.cs
+++ Traectory/Program.cs
@@ -1,35 +1,72 @@
-﻿using System;
+﻿//Программу брал у Андрея Речкалова
+using System;
+using System.IO;
 
-namespace Traectory
+namespace Trajectory
 {
-    class Program
+    class Trajectory1
     {
-		
-static void Main(string[] args)
+        public double speed, time, x, y, angle, steps;
+        string path;
+        public Trajectory1()
         {
-			double x, y, xmax; //координаты
-			double t, v, a; //время, скорость, угол
-			Console.WriteLine("введите время");
-			t = Convert.ToDouble( Console.ReadLine());
-			Console.WriteLine("введите скорость");
-			v = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("введите угол");
-			a = Convert.ToDouble(Console.ReadLine());
-			//x - координата по горизонтали
-			x = v * Math.Cos(a * (Math.PI / 180)) * t;
-			//y - координаты по вертикали
-			y = v * Math.Sin(a * (Math.PI / 180)) * t - 9.8 * t*t / 2;
-			xmax = v*v * Math.Sin(2 * a * (Math.PI / 180)) / 9.8;
-			if (x > xmax)
-			{
-				Console.WriteLine( "X равен: " + xmax );
-				Console.WriteLine("Y равен:0");
-			}
-			else
-			{
-				Console.WriteLine("X равен: " + x );
-				Console.WriteLine("Y равен: " + y);
-			}
+            Console.WriteLine("Введите название файла, у которого установлено расширение csv");
+            path = @"D:\visual_projects\sharp\Trajectory_1\" + Console.ReadLine() + ".csv";
+            speed = reading_from_file(path, 0);
+            angle = Math.PI * reading_from_file(path, 1) / 180;
+            time = reading_from_file(path, 2);
+            steps = reading_from_file(path, 3);
+        }
+        public double reading_from_file(string path, int i)
+        {
+            string line = null;
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                for (int j = 0; j <= i; ++j)
+                {
+                    if ((line = sr.ReadLine()) == null)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (line != null)
+                return Convert.ToDouble(line);
+            else return 0;
+        }
+        public void out_put(double time)
+        {
+            x = speed * Math.Cos(angle) * time;
+            y = speed * Math.Sin(angle) * time - 4.9 * time * time;
+            if (y <= 0)
+                y = 0;
+            using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+            {
+                sw.Write("\n" + "x(");
+                sw.Write("{0:0.##}", time);
+                sw.Write(") = ");
+                sw.Write("{0:0.##}", x);
+                sw.Write("\t" + "y(");
+                sw.Write("{0:0.##}", time);
+                sw.Write(") = ");
+                sw.Write("{0:0.##}", y);
+            }
+        }
+        public void cycle(double time, double steps)
+        {
+            for (double i = 0; i < time; i += steps)
+            {
+                out_put(i);
+            }
+            out_put(time);
+        }
+    }
+    class Trajectory
+    {
+        public static void Main(string[] args)
+        {
+            Trajectory1 trajectory = new Trajectory1();
+            trajectory.cycle(trajectory.time, trajectory.steps);
         }
     }
 }
